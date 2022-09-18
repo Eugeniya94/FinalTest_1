d011e4de9e1a11e7a8359cfa9d18f9f7d43f8be9
Evgeniya <sluchevskaya0309@yandex.ru>
2022-09-18 13:19:21 +0300

    Solve test

---
+++ Test/Program.cs
@@ -0,0 +1,29 @@
+﻿
+void CreateSecondArray(string[] fArray, string[] sArray)
+{
+    int count = 0;
+    for (int i = 0; i < fArray.Length; i++)
+    {
+    if(fArray[i].Length <= fArray.Length)
+        {
+        sArray[count] = fArray[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i == 0) Console.Write("[");
+        if (i < array.Length - 1) Console.Write(array[i] + ",");
+        else Console.Write(array[i] + "]");
+    }
+}
+string[] firstArray = new string[5] {"Oslo", "Quebec", "Seoul", "Mockow", "Dubai"};
+string[] secondArray = new string[3];
+
+CreateSecondArray(firstArray, secondArray);
+PrintArray(firstArray);
+Console.WriteLine();
+PrintArray(secondArray);
